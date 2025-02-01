93d1d0f3a6ac9aa4e39e368f351d08e3eb0ca564
nerdycore <ramosbryandm@gmail.com>
2025-02-01 10:27:13 +0800

    BSCS Student Registration
    Creating a Computer Science Student Registration using Java GUI and Java OOP

---
+++ studentInfo.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class studentInfo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("COMPUTER SCIENCE STUDENT REGISTRATION");
+        frame.setSize(500,500);
+        frame.setVisible(true);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }
+}
